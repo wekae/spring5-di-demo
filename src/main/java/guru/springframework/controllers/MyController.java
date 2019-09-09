a918d87ea733eef452584894992c21c10a9f1d66
Victor W <wekae.dev@gmail.com>
2019-09-09 12:50:59 +0300

    My Assignment Answer

--- src/main/java/guru/springframework/controllers/MyController.java
+++ src/main/java/guru/springframework/controllers/MyController.java
@@ -1,6 +1,7 @@
 package guru.springframework.controllers;
 
 import guru.springframework.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 /**
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class MyController {
 
+    @Autowired
     private GreetingService greetingService;
 
     public MyController(GreetingService greetingService) {
